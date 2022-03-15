7e1108ae9a242e3c7a25866b45e418f73e09d9cf
SashBFa <bastienfajs@gmail.com>
2022-03-15 07:46:29 +0100

    mobile version

--- assets/JS/index.js
+++
@@ -1,8 +0,0 @@
-/******************* Navigation ******************/
-
-let toggle = document.querySelector(".nav__toggle");
-let nav = document.querySelector(".nav");
-
-toggle.addEventListener("click", function(){
-    nav.classList.toggle("toggle__on");
-})
---
+++ assets/JS/parallax.js
@@ -0,0 +1,21 @@
+const simpleParallax = (elem, modifier) => {
+    let paras = document.getElementsByClassName(elem);
+    for (let i = 0; i < paras.length; i++) {
+        paras[i].setAttribute('style', 'background-repeat: no-repeat; background-attachment: fixed; background-position: center center;');
+    }
+    const sp = () => {
+        for (let i = 0; i < paras.length; i++) {
+            let x = paras[i].getBoundingClientRect().top / modifier;
+            let y = Math.round(x * 100) / 100;
+            paras[i].style.backgroundPosition = 'center ' + y + 'px';
+        }
+        requestAnimationFrame(sp);
+    }
+    requestAnimationFrame(sp);
+}
+simpleParallax('header',8);
+simpleParallax('counter',8);
+simpleParallax('symptoms',8);
+simpleParallax('update',8);
+simpleParallax('blog',8);
+simpleParallax('footer',8);
